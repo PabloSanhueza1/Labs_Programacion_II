2152fbbe424d54a575f83d17b3535006e7283fec
PabloSanhueza1 <Sanhueza.pem@gmail.com>
2022-09-10 21:02:21 -0300

    commit

--- Lab3/Ejercicio26Lab3/src/ejercicio26lab3/Ejercicio26Lab3.java
+++ Lab3/Ejercicio26Lab3/src/ejercicio26lab3/Ejercicio26Lab3.java
@@ -1,24 +1,50 @@
 package ejercicio26lab3;
+
 import java.util.ArrayList;
+
 public class Ejercicio26Lab3 {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        Deposito deposito = new Deposito();
+        Bebida b1 = new Bebida(1);
+        Bebida b2 = new Bebida(2);
+        Bebida b3 = new Bebida(3);
+        Bebida b4 = new Bebida(4);
+        deposito.addBebida(b1);
+        deposito.addBebida(b2);
+        deposito.addBebida(b3);
+        deposito.addBebida(b4);
+
+        Bebida b5 = deposito.getBebida();
+        Bebida b6 = deposito.getBebida();
+        
+        
     }
-    
+
 }
 
-class Deposito{
-    public Deposito(){
-        
+class Deposito {
+
+    private ArrayList<Bebida> deposito;
+
+    public Deposito() {
+        deposito = new ArrayList();
     }
-    public void addBebida(Bebida b){
-        
+
+    public void addBebida(Bebida b) {
+        deposito.add(b);
     }
-    public Bebida getBebida(){
-        return
+
+    public Bebida getBebida() {
+        if (deposito.size() == 0) {
+            return null;
+        } else {
+            Bebida b = deposito.get(0);
+            deposito.remove(0);
+            return b;
+        }
     }
-    
+
 }
 
 class Bebida {
@@ -44,14 +70,6 @@ class Bebida {
         return serie;
     }
 
-    public int beber(int b) {
-        if (contenido - b >= 0) {
-            contenido = contenido - b;
-            return b;
-        } else {
-            int aux = contenido;
-            contenido = 0;
-            return aux;
-        }
+    public void beber(int b) {
     }
 }
